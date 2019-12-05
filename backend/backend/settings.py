033c3bb7b6d8cff3514d076b5d4be7364be5542f
RobBlanchard <robin.blanchard00@gmail.com>
2019-12-05 16:05:59 +0100

    This may work

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 
 # we whitelist localhost:3000 because that's where frontend will be served
 CORS_ORIGIN_WHITELIST = [
@@ -132,5 +132,6 @@ CORS_ORIGIN_WHITELIST = [
 
 # Activate Django-Heroku.
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles')
+
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 django_heroku.settings(locals())
