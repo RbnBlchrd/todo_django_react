17df29175ac28a0c50468847028e32637d4fecf4
RobBlanchard <robin.blanchard00@gmail.com>
2019-12-05 11:42:18 +0100

    Ready to heroku this.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -128,3 +129,6 @@ CORS_ORIGIN_WHITELIST = [
         'http://localhost:3000'
     ]
 # CORS_ORIGIN_ALLOW_ALL = True
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
