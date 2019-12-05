c5e1595f57510ae3c4d98c231a0950298707d596
RobBlanchard <robin.blanchard00@gmail.com>
2019-12-05 12:29:30 +0100

    Now really ready to heroku this.

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+from django.core.handlers.wsgi import WSGIHandler
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.backend.settings')
 
-application = get_wsgi_application()
+application = WSGIHandler()
